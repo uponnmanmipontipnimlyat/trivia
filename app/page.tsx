67d676c5da09cb377a94c96f429aa294d98974c6
Uponnmanmi Pontip <uponnmanmipontipnimlyat@gmail.com>
2025-11-30 21:24:24 +0100

    whatver you do

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,8 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
      
-       <h1> Tamara's Fairview School</h1>
+       <h1> Tamaras Fairview School</h1>
+       <p> Tamaras fairview school is a school is a school where your child can get real quality education</p>
         
       </main>
     </div>
