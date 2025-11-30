b2ded85844525d243e5e35a950d01d3bdad5e382
Uponnmanmi Pontip <uponnmanmipontipnimlyat@gmail.com>
2025-11-30 21:58:52 +0100

    Whatever i say

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,18 @@ export default function Home() {
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
      
 
-      <div>
-        <img  alt=""  src="/img/logo.jpg"  />
-      </div>     
+
+    {/* Logo */}
+        <Image 
+          src="/logo.png"       // path inside public/
+          alt="Tamaras Fairview School Logo"
+          width={120}           // adjust size
+          height={120}
+          className="mb-6"
+        />
+          
       <h1> Tamaras Fairview School</h1>
-      <p> Tamaras fairview school is a school is a school where your child can get real quality education</p>
+       <p> Tamaras fairview school is a school is a school where your child can get real quality education</p>
         
       </main>
     </div>
